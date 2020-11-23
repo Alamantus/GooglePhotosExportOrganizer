744fc740c9024275558bf7bde63e1fb4d468a004
Robbie Antenesse <dev@alamantus.com>
2020-11-23 11:31:58 -0700

    Update Step1 text; Make sidebar fixed

--- src/Views/Step1.js
+++ src/Views/Step1.js
@@ -19,8 +19,19 @@ function Step1(props) {
     <article>
       <h1 className="type-h2">{ NAV_ITEMS[0] }</h1>
       <p className="type-p1">
-        Follow the instructions on <ExternalLink href={ blogLink }>this page</ExternalLink> to
-        export your Google Photos from Google Takeout. 
+        Follow the instructions for Step 1 on <ExternalLink href={ blogLink }>this page</ExternalLink> to
+        export your Google Photos from Google Takeout.
+      </p>
+      <p className="type-p2">
+        This will probably take a while to finish&mdash;just make sure that all of the <code>.zip</code> files
+        are downloaded into the same folder on your computer. That way, this program will be able
+        to find them all and do its job!<br />
+        <strong>IMPORTANT:</strong> Only create Google Takeout files for Google Photos and <em>NOT</em> any
+        other Google services! Using <code>.zip</code> files that are not formatted how Google Takeout provides
+        the Google Photos export may cause this program to behave unpredictably.
+      </p>
+      <p className="type-p2">
+        Once you're finished downloading all of the Google Takeout files, you can move to the next step.
       </p>
     </article>
   )
